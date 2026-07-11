56cc583d787bf378f0d333a86d97169a2d8effc7
pRIYa25S <priyadhashini122@gmail.com>
2026-07-11 17:08:58 +0530

    fix: simplify database module to clear compile error

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,9 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-// Change 'dynamic' to 'static' right here:
-import { DATABASE_URL } from '$env/static/private';
-
+// Use the standard process.env which Vercel reads perfectly automatically
 export const pool = new Pool({
-    connectionString: DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
