601415675e7b9bc694ef1649e317ca3a666e6ed7
pRIYa25S <priyadhashini122@gmail.com>
2026-07-18 21:52:51 +0530

    fix: empty active db connection for deployment

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,1 @@
-import pg from 'pg';
-const { Pool } = pg;
-
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'chessdb',
-    password: 'Mani@25.',  // Your original password with the special character
-    port: 5432,
-});
+export const pool = {};
