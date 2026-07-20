28a42ff8b4781aae8a4b964b1698fb3d9e31f91a
pRIYa25S <priyadhashini122@gmail.com>
2026-07-20 13:10:42 +0530

    fix: update svelte.config preprocess import

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
